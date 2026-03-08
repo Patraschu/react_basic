1bdb08d7d6747afea23a11b16f53eb5a9f377b1f
Patraschu <sooho0411@gmail.com>
2026-03-08 20:26:19 +0900

    first commit

---
+++ src/component/Mynav.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class Mynav extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    console.log('shouldComponentUpdate 작동',
+      newProps.data,
+      this.props.data
+    );
+    if(this.props.data === newProps.data){
+      return false;
+    }
+
+    return true;
+  }
+  render() {
+    console.log('Mynav 실행');
+    let lists = [];
+    let data = this.props.data;
+
+    data.forEach(item => {
+      lists.push(
+        <li key={item.id}>
+          <a href="/"
+            data-id={item.id}
+            onClick={e => {
+              e.preventDefault();
+              this.props.onChangePage(item.id);
+            }}>
+            {item.title}</a></li>
+      )
+    });
+
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Mynav;
